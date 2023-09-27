6da2390a121da4db01f24b8d6814419792f53b1c
midnqp <midnqp@gmail.com>
2023-09-27 22:59:41 +0600

    latest changes

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
-require('dotenv/config')
 const axios = require('axios')
 const express = require('express')
-const cors = require('cors')
+const {neurelo} = require('./util.js')
 
-const apiKey = process.env.NEURELO_API_KEY
-const projectUrl = process.env.NEURELO_PROJECT_URL
+// 1. have the database in neurelo
+// 2. introspect the database
+// 3. have the backend app make api calls to the neurelo api url.
+
+// 1. install an orm
+// 2. configure the database in the orm
+// 3. make api calls to database to fetch data.
 
 const app = express()
 
-app.get('/address', async (req, res, next) => {
-    const response = await axios({
-        url: projectUrl + '/rest/address/1',
-        responseType: 'json', 
-        headers: {'x-api-key': apiKey}
-    })
-    console.log(response.data)
-    res.json({ok:true})
-    next()
+app.get('/address', async (req, res, next) => res.json({ok:true, data: await neurelo('/rest/address')}) && next())
+
+app.get('/address/:id', async (req, res, next) => res.json({ok:true, data: await neurelo('/rest/address/'+req.params.id)}) && next())
+
+app.post('/address', async (req, res, next) => res.json({ok:true, data: await neurelo('/rest/address/__one', req.body, 'post')}) && next())
+
+app.delete('/address/:id', async (req, res, next) => res.json({ok:true}))
+
+app.use(function (err, req, res, next) {
+    if (err) console.log('encountered error', err)
 })
 
-app.use(function (req, res, next, err) {
-    console.log('encountered error', err)
-})
-
-app.listen(8080, ()=>console.log("we're up"))
+app.listen(8081, ()=>console.log("we're up"))
